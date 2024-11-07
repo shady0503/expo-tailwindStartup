2d4406ac4f4f30de5aac930117cc4239a4c40733
Shady0503 <chaditaqi3@gmail.com>
2024-11-07 22:50:46 +0100

    initial commit

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import "../../global.css";
+
+
+const _Rootlayout = () => {
+  return (
+    <View className='flex justify-center items-center h-full w-full bg-gray-100
+    '>
+      <Text>
+        Hello, open the app folder and start editing the files to see changes.
+      </Text>
+    </View>
+  )
+}
+
+export default _Rootlayout
+
